96b66ca0cf8261f046052b5ba70bd0e9326a456e
luizmosciaro <edu.mosciaro@live.com>
2023-01-16 19:25:22 -0400

    Refix issues

--- application.py
+++ application.py
@@ -4,7 +4,8 @@ application = Flask(__name__)
 
 @application.route('/')
 def home():
-    return "Hello, this is my first deployment with Flask in Python"
+    return "Hello, this is my first deployment with Flask in Python\n \
+    Ola, essa é a minha primeira implementação com Flask em Python"
 
 if __name__=='__main__':
     application.run(debug=True)
