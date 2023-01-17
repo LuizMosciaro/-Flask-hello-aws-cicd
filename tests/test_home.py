a7022f16c30e91169a7ea428a814245b8263d8bb
luizmosciaro <edu.mosciaro@live.com>
2023-01-16 20:52:37 -0400

    fix test 2

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,23 +1,14 @@
-import pytest
 from application import application 
 
-@pytest.fixture()
-def app():
-    app = application
-    return app
+with application.test_client() as client:
+    response = client.get("/")
+    assert b"<h1>Hello, World!</h1>" in response.data
 
-def test_request_bytes(app):
-    with app.test_client() as client:
-        response = client.get("/")
-        assert b"<h1>Hello, World!</h1>" in response.data
+with application.test_client() as client:
+    response = client.get("/")
+    assert "<h1>Hello, World!</h1>" in response.text
 
-def test_request_text(app):
-    with app.test_client() as client:
-        response = client.get("/")
-        assert "<h1>Hello, World!</h1>" in response.text
-
-def test_request_status_code(app):
-    with app.test_client() as client:
-        response = client.get("/")
-        assert response.status_code == 200
+with application.test_client() as client:
+    response = client.get("/")
+    assert response.status_code == 200
     
