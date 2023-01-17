674ea64b35562c538a91d9661465565e00e5f3ba
luizmosciaro <edu.mosciaro@live.com>
2023-01-16 20:33:30 -0400

    writing tests

--- application.py
+++ application.py
@@ -4,8 +4,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def home():
-    return "Hello, this is my first deployment with Flask in Python\n \
-    Ola, essa é a minha primeira implementação com Flask em Python"
+    return "<h1>Hello, World!</h1>"
 
 if __name__=='__main__':
     application.run(debug=True)
---
+++ tests/test_home.py
@@ -0,0 +1,24 @@
+import pytest
+from application import application
+
+@pytest.fixture()
+def app():
+    app = application
+    return app
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+def test_request_bytes(client):
+    response = client.get("/")
+    assert b"<h1>Hello, World!</h1>" in response.data
+
+def test_request_text(client):
+    response = client.get("/")
+    assert "<h1>Hello, World!</h1>" in response.text
+
+def test_request_status_code(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    
